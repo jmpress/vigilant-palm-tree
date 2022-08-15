98c3af58e3be048d729db80c840fa87df3cc6ce6
Jeff <j.michael.press@gmail.com>
2022-08-14 18:33:50 -0700

    user reg page works, builds user object sends to API

--- public/newUser.js
+++ public/newUser.js
@@ -8,46 +8,40 @@ let headers = {
 const currDate = document.getElementById('currDate');
 
 //Input field variables
-const newPriority = document.getElementById('newPriority');
-const newSubj = document.getElementById('newSubj');
-const newDetails = document.getElementById('newDetails');
-const newRegarding = document.getElementById('regarding');
+const userEmail = document.getElementById('userEmail');
+const userPassA = document.getElementById('userPassA');
+const userPassB = document.getElementById('userPassB');
 
 //Button variables
-const newTicketButton = document.getElementById('newTicket');
+const registerButton = document.getElementById('register');
 const cancelNewButton = document.getElementById('cancelNew');
 
 //Page Load actions
 currDate.innerHTML = todaysDate();
 
 //eventHandlers
-newTicketButton.addEventListener('click', () => {
-    
-    
-    newTx = {
-        ticket_id: 0,
-        open_date: todaysDate(),
-        close_date: null,
-        ticket_priority: newPriority.value,
-        ticket_status: 1,
-        ticket_subject: newSubj.value,
-        ticket_description: newDetails.value,
-        ticket_from: newRegarding.value,
-        opener_id: 1,
-        closer_id: null
-    };
-    postNewTicket(newTx);
+registerButton.addEventListener('click', () => {
+    if(checkSamePass()){
+        newUser = {
+            u_id: 0,
+            u_email: userEmail.value,
+            plain_pass: userPassA.value,
+            num_tix_closed: 0
+        };
+        console.log(newUser);
+        postNewUser(newUser);
+    }
 });
 
 cancelNewButton.addEventListener('click', () => {
-    window.location.href = './inbox.html';
+    window.location.href = './index.html';
 });
 
 
 //fetch request
-async function postNewTicket(newTx){
-    //fetch POST request with appropriate headers (new Ticket object in body)
-    const response = await fetch(`/newTicket`, {method: 'POST', headers: headers, body: JSON.stringify(newTx)});
+async function postNewUser(newUser){
+    //fetch POST request with appropriate headers (new User object in body)
+    const response = await fetch(`/newUser`, {method: 'POST', headers: headers, body: JSON.stringify(newUser)});
     if(response.ok){
         console.log('Should be good, check the DB');
         window.location.href = './inbox.html'
@@ -64,4 +58,14 @@ let cMonth = currentDate.getMonth() + 1;
 let cYear = currentDate.getFullYear();
 let dateString = `${cYear}-${cMonth}-${cDay}`;
 return dateString;
+}
+
+function checkSamePass(){
+    if(userPassA.value !=  userPassB.value || userPassA.value === '' || userPassB.value === ''){
+        window.alert('Passwords must exist and match!')
+        return false;
+    } else {
+        return true;
+    }
+    
 }

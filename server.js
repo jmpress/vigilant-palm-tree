9b2f9ec54b61c8bc0143a5a0bcc79e25283c42b7
Jeff <j.michael.press@gmail.com>
2022-09-04 17:05:37 -0700

    messing with heroku again

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 1000*60*60, secure: false, sameSite: 'none' },
-  //secure: true,  when in production, make it true.
+  secure: true,  //when in production, make it true.
   store
 })
 )
